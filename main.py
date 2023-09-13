4083ab2b884db851a0a2873e3e95315542d3e247
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-13 11:14:07 +0200

    Fixar kommentareer

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ class Spelare:
         return " : ".join(map(str, self.hand))  # konverterar till sträng för att visa kort
 
 
-class TjugoettSpel: # börjar på tjogoettklasss
+class TjugoettSpel: # börjar på tjogoettklass
     def __init__(self):
         self.kortlek = Kortlek()
+
+
