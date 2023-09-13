3e2b4f38745e6fdbf406e734fb1b151983087243
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-13 10:44:41 +0200

    skapar classen spelare som har en tom hand för spelare, lägger till kort i spelaren hand, räknar poäng, gör s till 1 om det överstiger 21 i spelarens hand och 14 om den inte gör.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random  #Importera random modul för att blanda kortleken.
 
+
 class Kortlek:
    def __init__(self):
        # skapa en ny kortlek, genom att multiplicera varje kortvärde med 4 hjärter ruter klöver och spader
@@ -9,4 +10,23 @@ class Kortlek:
 
    def dra_kort(self):
        return self.kortlek.pop()  #dra kort från kortlek och retunera det
+
+
+class Spelare:
+    def __init__(self):
+        self.hand = []  #skapad en tom hand för spelaren alltså jag
+
+    def drakort(self, kort):
+        self.hand.append(kort)  #lägger till kort i spelarens hand
+
+    def rakna_poang(self):
+        poang = sum(self.hand)  #räkna poäng genom att sumera värderna i handen
+        if 14 in self.hand and poang > 21:
+            self.hand.remove(14)
+            self.hand.append(1)  #om jag har ett S som är 14 och det går över 21 då räknas det som 1 istället
+            poang = sum(self.hand)
+        return poang
+
+    def visa_hand(self):
+        return " : ".join(map(str, self.hand))  #konverterar till sträng för att visa kort
 
